5337a18b61a631fffd29f3e1da58aacd27212323
NadiaMok <NadiaMok@users.noreply.github.com>
2019-01-17 12:26:17 -0500

    Some changes to Create Page
    I edited the create page a bit

--- resources/views/createnew.blade.php
+++ resources/views/createnew.blade.php
@@ -3,34 +3,30 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-8 col-md-offset-4">
                 <div class="panel panel-default">
                     <div class="panel-heading">
+                        <div class="card-header h2 ">Create An Event:</div>
+                        <div class="card-body">
+                            <form action="/create" method="post">
+                                    <label> Event Manager</label>
+                                    <input type="text" class="form-control"  name="eventOwner" placeholder="Event Owner">
+                                    <label> Full Event Title </label>
+                                    <input type="text" class="form-control" name="eventName" placeholder="e.g. Master-Class">
+                                    <label> Dates & Times </label>
+                                    <input type="date" class="form-control" name="eventDate1">
+                                    <input type="date" class="form-control" name="eventDate2">
+                                    <label> Location & Room </label>
+                                    <input type="text" class="form-control" name="eventLocation" placeholder="e.g. Casa-Loma campus, C418">
+                                    <label> Max. Participants </label>
+                                    <input type="text" class="form-control" name="maxNumParticipants" placeholder="e.g. 100">
+                                    <label> Detailed Description</label>
+                                    <input type="text" class="form-control" name="eventDescription" placeholder="e.g. Unexpectedly sharp and bright seminar">
 
-                        <div class="panel-heading"><h2>&nbsp&nbsp&nbsp&nbsp Create An Event:</h2></div><br/>
-                        <form action="/create" method="post">
-
-                            <label> Event Manager</label>&nbsp&nbsp&nbsp
-                            <input type="text" name="eventOwner" value="Some Owner"><br /><br>
-                            <label> Full Event Title </label>&nbsp&nbsp&nbsp
-                            <input type="text" name="eventName" value="Master-Class"><br /><br>
-                            <label> Dates & time </label>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-                            <input type="text" name="eventDate1" value="Wed., January 2, 12:00pm">&nbsp&nbsp&nbsp&nbsp
-                            <input type="text" name="eventDate2" value="Th., January 3, 2:00pm"><br /><br>
-                            <label> Location& room </label>&nbsp&nbsp
-                            <input type="text" name="eventLocation" value="GBC Casa-Loma campus, C418"><br /><br>
-                            <label> Max. participants </label>
-                            <input type="text" name="maxNumParticipants" value="100"><br /><br>
-                            <label> Detailed descript.</label>
-                            <input type="text" name="eventDescription" value="Unexpectedly sharp and bright seminar"><br /><br>
-
-                            {{csrf_field()}}
-                            <a href="/home"><button>Reset</button></a>&nbsp&nbsp&nbsp&nbsp
-
-                            <input type="submit" name="Submit" value="Add record">
-                        </form>
-
+                                    {{csrf_field()}}
+                                    <a href="/home"><button class="btn btn-success">Reset</button></a>&nbsp&nbsp&nbsp&nbsp
+                                    <input type="submit" class="btn btn-success" name="Submit" value="Add record">
+                            </form>
+                        </div>
                     </div>
-
-
 @endsection
